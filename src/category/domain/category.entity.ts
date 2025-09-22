0a40eadd54262762727b9c6104c3d350a7f40f7a
Matheus Sena Rosa <matheussenarosa8@gmail.com>
2025-09-22 11:27:06 -0300

    feat: add data test builder

--- src/category/domain/category.entity.ts
+++ src/category/domain/category.entity.ts
@@ -2,6 +2,7 @@ import { Entity } from "../../shared/domain/entity";
 import { EntityValidationError } from "../../shared/domain/errors/validation.error";
 import { ValueObject } from "../../shared/domain/value-object";
 import { Uuid } from "../../shared/domain/value-objects/uuid.vo";
+import { CategoryFakeBuilder } from "./category-fake.builder";
 import { CategoryValidatorFactory } from "./category.validator";
 
 type CategoryConstructorProps = {
@@ -53,6 +54,10 @@ export class Category extends Entity {
     if (!isValid) {
       throw new EntityValidationError(validator.errors);
     }
+  }
+
+  static fake() {
+    return CategoryFakeBuilder;
   }
 
   changeName(name: string): void {
