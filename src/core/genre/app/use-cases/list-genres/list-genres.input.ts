b7dd71049d2c940aa58f7670f331729f5e9ddbed
Matheus Sena Rosa <matheussenarosa8@gmail.com>
2025-10-06 08:48:28 -0300

    feat: add genre use cases

---
+++ src/core/genre/app/use-cases/list-genres/list-genres.input.ts
@@ -0,0 +1,25 @@
+import { SearchInput } from "../../../../shared/app/search-input";
+import { SortDirection } from "../../../../shared/domain/repository/search-params";
+import { IsArray, IsUUID, ValidateNested, validateSync } from "class-validator";
+
+export class ListGenresSearchTerm {
+  name?: string;
+  @IsUUID("4", { each: true })
+  @IsArray()
+  categories_id?: string[];
+}
+
+export class ListGenresInput implements SearchInput<ListGenresSearchTerm> {
+  page?: number;
+  per_page?: number;
+  sort?: string;
+  sort_dir?: SortDirection;
+  @ValidateNested()
+  searchTerm?: ListGenresSearchTerm;
+}
+
+export class ValidateListGenresInput {
+  static validate(input: ListGenresInput) {
+    return validateSync(input);
+  }
+}
