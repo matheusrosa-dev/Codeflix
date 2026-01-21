4321a1d9f79b7c6a83260e66b18ddd3cd38d33b8
Matheus Sena Rosa <matheussenarosa8@gmail.com>
2026-01-20 21:31:54 -0300

    feat: integrate Google Cloud Storage for media handling
    - Added @google-cloud/storage dependency to package.json.
    - Implemented GoogleCloudStorage class for storing and retrieving files.
    - Created integration and unit tests for GoogleCloudStorage.
    - Developed ProcessAudioVideoMediasInput and ProcessAudioVideoMediasUseCase for processing media uploads.
    - Implemented UploadAudioVideoMediaInput and UploadAudioVideoMediasUseCase for handling audio/video uploads.
    - Added validation for media inputs using class-validator.
    - Updated banner location format in Banner value object to use "images" instead of "imagens".

---
+++ src/core/video/app/use-cases/process-audio-video-medias/process-audio-video-medias.input.ts
@@ -0,0 +1,47 @@
+import {
+  IsNotEmpty,
+  IsString,
+  IsUUID,
+  IsIn,
+  validateSync,
+} from "class-validator";
+import { Status } from "../../../../shared/domain/value-objects/audio-video-media.vo";
+
+export type ProcessAudioVideoMediasInputConstructorProps = {
+  video_id: string;
+  encoded_location: string;
+  field: "trailer" | "video";
+  status: Status;
+};
+
+export class ProcessAudioVideoMediasInput {
+  @IsUUID("4")
+  @IsString()
+  @IsNotEmpty()
+  video_id: string;
+
+  @IsNotEmpty()
+  encoded_location: string;
+
+  @IsIn(["trailer", "video"])
+  @IsNotEmpty()
+  field: "trailer" | "video";
+
+  @IsIn([Status.COMPLETED, Status.FAILED])
+  @IsNotEmpty()
+  status: Status;
+
+  constructor(props?: ProcessAudioVideoMediasInputConstructorProps) {
+    if (!props) return;
+    this.video_id = props.video_id;
+    this.encoded_location = props.encoded_location;
+    this.field = props.field;
+    this.status = props.status;
+  }
+}
+
+export class ValidateProcessAudioVideoMediasInput {
+  static validate(input: ProcessAudioVideoMediasInput) {
+    return validateSync(input);
+  }
+}
