eafc9d46cc5beebbb74aab83f569ee7c5286866c
Matheus Sena Rosa <matheussenarosa8@gmail.com>
2026-02-11 18:35:50 -0300

    feat: refactor event handler interfaces and implement integration event for video media replacement

--- src/core/video/domain/domain-events/video-audio-media-replaced.event.ts
+++ src/core/video/domain/domain-events/video-audio-media-replaced.event.ts
@@ -1,12 +1,15 @@
-import { IDomainEvent } from "../../../shared/domain/events/domain-event.interface";
-import { Trailer } from "../trailer.vo";
-import { VideoMedia } from "../video-media.vo";
-import { VideoId } from "../video.aggregate";
+import {
+  IDomainEvent,
+  IIntegrationEvent,
+} from '../../../shared/domain/events/domain-event.interface';
+import { Trailer } from '../trailer.vo';
+import { VideoMedia } from '../video-media.vo';
+import { VideoId } from '../video.aggregate';
 
 type VideoAudioMediaReplacedProps = {
   aggregate_id: VideoId;
   media: Trailer | VideoMedia;
-  media_type: "trailer" | "video";
+  media_type: 'trailer' | 'video';
 };
 
 export class VideoAudioMediaReplaced implements IDomainEvent {
@@ -15,7 +18,7 @@ export class VideoAudioMediaReplaced implements IDomainEvent {
   event_version: number;
 
   readonly media: Trailer | VideoMedia;
-  readonly media_type: "trailer" | "video";
+  readonly media_type: 'trailer' | 'video';
 
   constructor(props: VideoAudioMediaReplacedProps) {
     this.aggregate_id = props.aggregate_id;
@@ -23,5 +26,27 @@ export class VideoAudioMediaReplaced implements IDomainEvent {
     this.media_type = props.media_type;
     this.occurred_on = new Date();
     this.event_version = 1;
+  }
+
+  getIntegrationEvent(): VideoAudioMediaUploadedIntegrationEvent {
+    return new VideoAudioMediaUploadedIntegrationEvent(this);
+  }
+}
+
+export class VideoAudioMediaUploadedIntegrationEvent
+  implements IIntegrationEvent
+{
+  event_name: string;
+  payload: any;
+  event_version: number;
+  occurred_on: Date;
+  constructor(event: VideoAudioMediaReplaced) {
+    this.event_version = event.event_version;
+    this.occurred_on = event.occurred_on;
+    this.payload = {
+      video_id: event.aggregate_id.id,
+      media: event.media.toJSON(),
+    };
+    this.event_name = this.constructor.name;
   }
 }
